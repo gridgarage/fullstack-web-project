d3cc4f9254056a0fb99fdb68573cd41d48c19c49
cgoarquitectura <cgoarquitectura@gmail.com>
2020-07-18 16:45:06 -0400

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,23 +2,24 @@ var gulp = require('gulp'),
  watch = require('gulp-watch'),
  postcss = require('gulp-postcss'),
  autoprefixer = require('autoprefixer'),
+ cssImport = require('postcss-import'),
  sass = require('gulp-sass');
-
 
 gulp.task('default', async function(){
     console.log("Horray, gulp is working"); 
 });
 gulp.task('html',function(){
-    return console.log("Html version is 5");
+    return gulp.src('./index.html')
+    .pipe(gulp.dest('./dist'));
+    
 });
 
 gulp.task('styles',function(){
     return gulp.src('./src/assets/scss/style.scss')
         .pipe(sass())
-        .pipe(postcss([autoprefixer]))
+        .pipe(postcss([cssImport, autoprefixer]))
         .pipe(gulp.dest('./dist'));
 });
-
 
 gulp.task('watch',function (){
        watch ('./*.html', gulp.series ('html'));
